ff322826855e58dff638f2adb6b2e1f1558664cb
WillianRGoncalves <willian.goncalves0x@gmail.com>
2016-10-29 12:33:09 -0200

    Shellsort finalizado

--- ShellSort.c
+++ ShellSort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void shell_sort (int *a, int n) {
+void shellsort (int a[], int n) {
     int gap, i, j, aux;
     for (gap = n; gap>=1; gap = gap / 2) {
         for (i = gap; i < n; i++) {
@@ -19,7 +19,7 @@ int main () {
     for (i = 0; i < 10; i++)
         printf("%d  ", a[i]);
     printf("\nConjunto Ordenado:\t");
-    shell_sort(a, 10);
+    shellsort(a, 10);
     for (i = 0; i < 10; i++)
         printf("%d  ", a[i]);
     return 0;
